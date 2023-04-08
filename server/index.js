44b12e501a13bc097c8429995d8e292fe9de2e3e
kerrycull <kerrywcull@hotmail.com>
2023-04-08 09:05:33 +0100

    ll

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const axios = require("axios");
 const mongoose = require("mongoose");
 require("dotenv").config({ path: "./config.env" });
-const port = process.env.PORT || 5000;
+const port = process.env.PORT;
 app.use(cors());
 app.use(express.json());
 app.use(require("./routes/record"));
